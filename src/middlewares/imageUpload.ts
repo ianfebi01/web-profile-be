77d0b2b3260c4bfb07dec20a9b05196a20b46d29
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2023-11-21 14:39:47 +0700

    change image upload validation

--- src/middlewares/imageUpload.ts
+++ src/middlewares/imageUpload.ts
@@ -1,13 +1,16 @@
 import removeTmp from "@/utils/removeTmp"
 import { RequestHandler } from "express"
 import * as status from "http-status"
-import createHttpError from 'http-errors'
 import createResponseError from "@/utils/createResponseError"
 
 const imageUpload: RequestHandler = async( req, res, next ) => {
 	try {
 		if ( !req.files || Object.values( req.files ).flat().length === 0 ) {
-			throw createHttpError( status.BAD_REQUEST, 'No files selected.' ) 
+			// throw createHttpError( status.BAD_REQUEST, 'No files selected' ) 
+			return res.status( status.BAD_REQUEST ).json( {
+				message : "No files selected",
+				status  : status.BAD_REQUEST
+			} )
 		}
 		const files = Object.values( req.files ).flat()
 		files.forEach( ( file ) => {
@@ -15,20 +18,29 @@ const imageUpload: RequestHandler = async( req, res, next ) => {
 				file.mimetype !== 'image/jpeg' &&
                 file.mimetype !== 'image/png' &&
                 file.mimetype !== 'image/gif' &&
-                file.mimetype !== 'image/svg+xml' &&
-                file.mimetype !== 'image/webp'
+                file.mimetype !== 'image/webp' &&
+                file.mimetype !== 'image/svg+xml'
 			) {
 				removeTmp( file.tempFilePath )
 
-				throw createHttpError( status.BAD_REQUEST, 'Unsupported format.' ) 
+				return res.status( status.BAD_REQUEST ).json( {
+					message : "Unsupported format",
+					status  : status.BAD_REQUEST
+				} )
 			}
 			if ( file.size > 1024 * 1024 * 4 ) {
 				removeTmp( file.tempFilePath )
 
-				throw createHttpError( status.BAD_REQUEST, 'File size is too large.' ) 
+				// throw createHttpError( status.BAD_REQUEST, 'File size is too large' ) 
+				return res.status( status.BAD_REQUEST ).json( {
+					message : "File size is too large",
+					status  : status.BAD_REQUEST
+				} )
 			}
 		} )
-		next()
+		if ( ! res.headersSent ) {
+			next()
+		}
 	} catch ( error: unknown ) {
 		createResponseError( res, error )
 	}
